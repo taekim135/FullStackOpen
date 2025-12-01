b91662e67ccd6d3e492b6e09a0106b73502be0e6
taekim135 <tkim363@uwo.ca>
2025-12-02 00:28:58 +0900

    error catching

--- part2/Countries/src/services/weather.js
+++ part2/Countries/src/services/weather.js
@@ -3,12 +3,14 @@ import axios from "axios";
 const api_key = import.meta.env.VITE_SOME_KEY
 
 
-
 const getWeather = (city) =>{
     const url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${api_key}&units=metric` 
 
     return axios.get(url)
     .then(response => response.data)
+    .catch(error =>{
+        console.log('Failed to fetch weather from '+ city, error);
+    })
 
 }
 
