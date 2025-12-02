1cb0c72b845c4ff2854202fa7886ace35fe63854
taekim135 <tkim363@uwo.ca>
2025-12-02 11:14:44 +0900

    weather feature (done)

--- part2/Countries/src/components/Country.jsx
+++ part2/Countries/src/components/Country.jsx
@@ -1,10 +1,18 @@
 import weatherAPI from "../services/weather";
-
-
-const weather = weatherAPI.getWeather("Seoul")
-console.log(weather)
+import { useState } from "react";
 
 const Country = (prop) =>{
+    const [temp, setTemp] = useState(0)
+    const [wind, setWind] = useState(0)
+
+    weatherAPI
+    .getWeather(prop.capital)
+    .then(data => {
+        //console.log(data)
+        setTemp(data.main.temp)
+        setWind(data.wind.speed)
+    })
+
     if (prop.length < 10 && prop.length > 1){
         return  <ul><li>{prop.name} <button onClick ={()=>prop.onClick(prop.name)}>Show</button></li></ul>
     }
@@ -12,11 +20,14 @@ const Country = (prop) =>{
     return(  
         <>
             <h1>{prop.name}</h1>
-            Capital: {prop.capital.map(cap => <ul key ={cap}><li>{cap}</li></ul>)}
-            <p>Area: {prop.area}</p>
-            Languages: {Object.values(prop.languages).map(lang => <ul key ={lang}><li>{lang}</li></ul>)}
-            <p>Population: {prop.population}</p>
+            <h3>Capital: {prop.capital.map(cap => <ul key ={cap}><li>{cap}</li></ul>)}</h3>
+            <h3>Area: {prop.area}</h3>
+            <h3>Languages: {Object.values(prop.languages).map(lang => <ul key ={lang}><li>{lang}</li></ul>)}</h3>
+            <h3>Population: {prop.population}</h3>
             <h1>{prop.flag}</h1>
+            <h3>Weather in {prop.capital}</h3>
+            <p>Temperature: {Math.round(Number(temp))+'\u00B0C'}</p>
+            <p>Wind: {Math.round(Number(wind))+'km/h'}</p>
         </> 
     ) 
 }
