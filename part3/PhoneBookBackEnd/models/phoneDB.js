3a5e71ed4ee67c0161fee7a6523d43c69960df19
taekim135 <tkim363@uwo.ca>
2026-01-06 00:43:59 +0900

    lint updated

--- part3/PhoneBookBackEnd/models/phoneDB.js
+++ part3/PhoneBookBackEnd/models/phoneDB.js
@@ -1,49 +1,49 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose")
 
-mongoose.set('strictQuery', false)
+mongoose.set("strictQuery", false)
 
 // connection url
 const url = process.env.PHONEDB
 
 mongoose.connect(url, { family: 4 })
-  .then(result => {
-    console.log('Connected to Phone MongoDB')
-  })
-  .catch(error => {
-    console.log('error connecting to Phone MongoDB:', error.message)
-  })
+    .then(() => {
+        console.log("Connected to Phone MongoDB")
+    })
+    .catch(error => {
+        console.log("error connecting to Phone MongoDB:", error.message)
+    })
 
 const phoneSchema = new mongoose.Schema({
 
-  name: {
-    type: "String",
-    minLength: 3,
-    required: true
-  },
-  number: {
-    type: "String",
-    validate: {
-      validator: function(v) {
-        return /\d{2}-/.test(v);
-      },
-      message: props => `${props.value} is not a valid phone number!`
+    name: {
+        type: "String",
+        minLength: 3,
+        required: true
     },
-    required: true
-  }
+    number: {
+        type: "String",
+        validate: {
+            validator: function(v) {
+                return /\d{2}-/.test(v)
+            },
+            message: props => `${props.value} is not a valid phone number!`
+        },
+        required: true
+    }
 })
 
 // format data output
-phoneSchema.set('toJSON', {
-  transform: (document, returnedObject) => {
-    returnedObject.id = returnedObject._id.toString()
-    delete returnedObject._id
-    delete returnedObject.__v
-  }
+phoneSchema.set("toJSON", {
+    transform: (document, returnedObject) => {
+        returnedObject.id = returnedObject._id.toString()
+        delete returnedObject._id
+        delete returnedObject.__v
+    }
 })
 
 // export default {} but model style
 // model name Note => collection name notes (small caps + plural)
-module.exports = mongoose.model('Contact', phoneSchema)
+module.exports = mongoose.model("Contact", phoneSchema)
 // model = constructor compiled
 // document = instance of a model (obj). contains data fields
 // collection = group of documents (students, teachers)
