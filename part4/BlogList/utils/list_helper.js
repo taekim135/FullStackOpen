8818b4afc765a98462cec4c17f0955379343b3d0
taekim135 <tkim363@uwo.ca>
2026-01-11 13:51:15 +0900

    most blog + empty condition

--- part4/BlogList/utils/list_helper.js
+++ part4/BlogList/utils/list_helper.js
@@ -1,12 +1,66 @@
 // helper methods
 // imported bt testing files (.test.js)
-const _ = require('lodash/core')
+const _ = require('lodash')
+
+// purely for testing
+const blogs = [
+  {
+    _id: "5a422a851b54a676234d17f7",
+    title: "React patterns",
+    author: "Michael Chan",
+    url: "https://reactpatterns.com/",
+    likes: 7,
+    __v: 0
+  },
+  {
+    _id: "5a422aa71b54a676234d17f8",
+    title: "Go To Statement Considered Harmful",
+    author: "Edsger W. Dijkstra",
+    url: "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
+    likes: 5,
+    __v: 0
+  },
+  {
+    _id: "5a422b3a1b54a676234d17f9",
+    title: "Canonical string reduction",
+    author: "Edsger W. Dijkstra",
+    url: "http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html",
+    likes: 12,
+    __v: 0
+  },
+  {
+    _id: "5a422b891b54a676234d17fa",
+    title: "First class tests",
+    author: "Robert C. Martin",
+    url: "http://blog.cleancoder.com/uncle-bob/2017/05/05/TestDefinitions.htmll",
+    likes: 10,
+    __v: 0
+  },
+  {
+    _id: "5a422ba71b54a676234d17fb",
+    title: "TDD harms architecture",
+    author: "Robert C. Martin",
+    url: "http://blog.cleancoder.com/uncle-bob/2017/03/03/TDD-Harms-Architecture.html",
+    likes: 0,
+    __v: 0
+  },
+  {
+    _id: "5a422bc61b54a676234d17fc",
+    title: "Type wars",
+    author: "Robert C. Martin",
+    url: "http://blog.cleancoder.com/uncle-bob/2016/05/01/TypeWars.html",
+    likes: 2,
+    __v: 0
+  }  
+    ]
 
 const dummy = (blogs) => {
   return 1
 }
 
 const totalLikes = (blogs) =>{
+  if (!blogs || blogs.length == 0) return null
+  
   let total = 0
 
   blogs.forEach(blog => {
@@ -17,6 +71,8 @@ const totalLikes = (blogs) =>{
 }
 
 const favoriteBlog = (blogs) => {
+  if (!blogs || blogs.length == 0) return null
+
   let best = blogs[0]
   
   blogs.forEach(blog => {
@@ -29,10 +85,28 @@ const favoriteBlog = (blogs) => {
 }
 
 
+const mostBlogs = (blogs) => {
+  if (!blogs || blogs.length == 0) return null
 
+  const counted = _.countBy(blogs, "author")
+   
+  // resut into an array
+  // entries = convert object into array of key-value pair
+  // map author & count key-value pair into object properties
+  const arrayForm = Object.entries(counted).map(([author,count]) => ({
+    author: author,
+    blogs: count
+  }))
+
+
+  return _.maxBy(arrayForm, "blogs")
+}
+
+console.log(mostBlogs(blogs))
 
 module.exports = {
   dummy,
   totalLikes,
-  favoriteBlog
+  favoriteBlog,
+  mostBlogs
 }
