e3f19d729eb0679c098bfb7b0b5eb02e495b41ea
taekim135 <tkim363@uwo.ca>
2026-01-14 11:04:32 +0900

    user integration

---
+++ part4/BlogList/model/user.js
@@ -0,0 +1,24 @@
+const mongoose = require("mongoose")
+
+const userSchema = new mongoose.Schema({
+    'username': {
+        type: String,
+        required: true,
+        unique: true
+    },
+    'name': String,
+    'passwordHash': String
+})
+
+usherSchema.set(toJSON, {
+    transform: (document, returnedObject) => {
+        returnedObject.id = returnedObject._id.toString()
+        delete returnedObject.__v
+        delete returnedObject._id
+
+        // WE DO NOT EXPOSE PW WHEN WORKING WITH API CALLS & RESPONSE DATA
+        // ONLY STORE THEM IN DB
+        delete returnedObject.passwordHash
+    }
+})
+
