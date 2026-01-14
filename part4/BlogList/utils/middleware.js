aa02b60fcee01e18cc10fc035b5e6a3eb978e821
taekim135 <tkim363@uwo.ca>
2026-01-14 22:13:53 +0900

    error handling for duplicated username

--- part4/BlogList/utils/middleware.js
+++ part4/BlogList/utils/middleware.js
@@ -7,9 +7,10 @@ const errorHandler = (error, request, response, next) => {
 
     if (error.name === "ValidationError") {
         response.status(400).json({"error" : error.message})
-    }
-    if (error.name === "CastError"){
+    }else if (error.name === "CastError"){
         response.status(400).send({"error" : "malformatted id"})
+    }else if (error.name === 'MongoServerError' && error.message.includes('E11000 duplicate key error')) {
+        response.status(400).json({ error: 'expected `username` to be unique' })
     }
     next(error)
 }
