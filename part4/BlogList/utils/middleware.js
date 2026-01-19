ae55aabb72c73e6b82b32f0a822250ca40d74e28
taekim135 <tkim363@uwo.ca>
2026-01-19 11:02:06 +0900

    errors updated

--- part4/BlogList/utils/middleware.js
+++ part4/BlogList/utils/middleware.js
@@ -12,7 +12,11 @@ const errorHandler = (error, request, response, next) => {
         response.status(400).send({"error" : "malformatted id"})
     }else if (error.name === 'MongoServerError' && error.message.includes('E11000 duplicate key error')) {
         response.status(400).json({ error: 'expected `username` to be unique' })
-    }else if (error.name === "JsonWebTokenError")
+    }else if (error.name === "JsonWebTokenError"){
+        response.status(401).json({ error: 'Invalid Token' })
+    }else if (error.name === "TokenExpiredError"){
+        response.status(401).json({ error: 'Token Expired' })
+    }
     next(error)
 }
 
