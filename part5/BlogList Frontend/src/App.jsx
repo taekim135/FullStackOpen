8a0db73e44f33f5a0f39ed474428bb22f3aef8bb
taekim135 <tkim363@uwo.ca>
2026-01-25 01:08:14 +0900

    login, state handler, token

--- part5/BlogList Frontend/src/App.jsx
+++ part5/BlogList Frontend/src/App.jsx
@@ -1,9 +1,28 @@
 import { useState, useEffect } from 'react'
 import Blog from './components/Blog'
 import blogService from './services/blogs'
+import loginService from "./services/login"
 
 const App = () => {
   const [blogs, setBlogs] = useState([])
+  const [user, setUser]= useState(null)
+  const [username, setUsername] = useState("")
+  const [password,setPassword] = useState("")
+
+  const handleLogin = async (event) => {
+    event.preventDefault()
+
+    try{
+      const user = await loginService.login({username, password})
+      setUser(user)
+      blogService.setToken(user.token)
+      setUsername("")
+      setPassword("")
+    }catch{
+      console.log("Invalid credentials")
+    }
+  }
+
 
   useEffect(() => {
     blogService.getAll().then(blogs =>
@@ -11,9 +30,33 @@ const App = () => {
     )  
   }, [])
 
+  if (user === null){
+    return (
+      <div>
+        <h2>Part 5 - Blogs Login Page</h2>
+          <form onSubmit={handleLogin}>
+          <div>
+            <label>
+              username
+              <input type = "text" value = {username} onChange = {({ target }) => setUsername(target.value)}/>
+            </label>
+          </div>
+          <div>
+            <label>
+              password
+              <input type = "password" value = {password} onChange = {({ target }) => setPassword(target.value)}/>
+            </label>
+          </div>
+          <button type="submit">Login</button>
+        </form>
+      </div>
+    )
+  }
+
   return (
     <div>
-      <h2>blogs</h2>
+      <h2>Part 5 - Blogs Frontend</h2>
+      <h4>Welcome, {user.name}</h4>
       {blogs.map(blog =>
         <Blog key={blog.id} blog={blog} />
       )}
