213528b0a3730a80be72e786f915f6f7f6160391
taekim135 <tkim363@uwo.ca>
2026-01-26 00:36:40 +0900

    blog form compo separated

---
+++ part5/BlogList Frontend/src/components/BlogForm.jsx
@@ -0,0 +1,31 @@
+const BlogForm = ({onSubmit,
+                    title, 
+                    author, 
+                    url, 
+                    handleTitleChange,
+                    handleAuthorChange,
+                    handleUrlChange}) => {
+
+    return(
+        <div>
+            <h3>Create New Blog</h3>
+            <form onSubmit={onSubmit}>
+                <label>
+                    Title:
+                    <input type="text" value = {title} onChange={handleTitleChange} required/><br/>
+                </label>
+                <label>
+                    Author:
+                    <input type="text" value = {author} onChange={handleAuthorChange} required/><br/>
+                </label>
+                <label>
+                    URL:
+                    <input type="text" value = {url} onChange={handleUrlChange} required/><br/>
+                </label>
+                <button type="submit">Create</button>
+            </form>
+        </div>
+    )
+}
+
+export default BlogForm
