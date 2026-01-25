f218079134f330fa2b374aff6f0568c8b18b581b
taekim135 <tkim363@uwo.ca>
2026-01-26 00:36:57 +0900

    wrapper compo

---
+++ part5/BlogList Frontend/src/components/Togglable.jsx
@@ -0,0 +1,33 @@
+import { useState } from 'react'
+
+
+// acts like a wrapper component to hide/show whatever is between the tags
+// present box + lid
+const Togglable = (props) => {
+  const [visible, setVisible] = useState(false)
+
+  // hide children/login form when login button is visible
+  const hideWhenVisible = { display: visible ? 'none' : '' }
+  const showWhenVisible = { display: visible ? '' : 'none' }
+
+  const toggleVisibility = () => {
+    setVisible(!visible)
+  }
+
+  return (
+    <div>
+
+      <div style={hideWhenVisible}>
+        <button onClick={toggleVisibility}>{props.buttonLabel}</button>
+      </div>
+
+      {/* show children if visible */}
+      <div style={showWhenVisible}>
+        {props.children} {/*  <--- LoginForm rendered here */}
+        <button onClick={toggleVisibility}>cancel</button>
+      </div>
+    </div>
+  )
+}
+
+export default Togglable
