51f4a259deb734f988d1ac73ffd32554a06c326c
taekim135 <tkim363@uwo.ca>
2026-01-25 21:24:18 +0900

    Notification new

---
+++ part5/BlogList Frontend/src/components/Notification.jsx
@@ -0,0 +1,20 @@
+const Notification = ({message}) => {
+
+    if (message == null || message == "" || !message){
+        return null
+    }else if (message.includes("Invalid")){
+        return (
+            <div className="error">
+                {message}
+            </div>
+        )
+    }else if (message.includes("Successful") || message.includes("Bye") || message.includes("Saved")){
+        return (
+            <div className="pass">
+                {message}
+            </div>
+        )
+    }
+}
+
+export default Notification
