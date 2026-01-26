9c7527820dc17973d34a87f1dc06fdeec3e5eb0b
taekim135 <tkim363@uwo.ca>
2026-01-26 23:57:45 +0900

    show-hide button

--- part5/BlogList Frontend/src/components/Blog.jsx
+++ part5/BlogList Frontend/src/components/Blog.jsx
@@ -1,7 +1,38 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import { useState } from "react"
+import blogService from '../services/blogs'
+
+const Blog = ({ blog }) => {
+  const [showDetail, setShowDetail] = useState(false)
+
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+
+  const handleDetail = () => {
+    setShowDetail(!showDetail)
+  }
+
+  const handleLike= () => {
+    
+  }
+
+  return (
+    <div style={blogStyle}>
+      <div>
+        {blog.title} By {blog.author} <button onClick={handleDetail}>{showDetail ? "Cancel" : "Show"}</button>
+      </div>
+      {(showDetail && (
+        <div>
+          {blog.url} <br/> {blog.likes} <button onClick={handleLike}>like</button> <br/> {blog.author}
+        </div>
+      ))}
+    </div>
+
+  )  
+}
 
 export default Blog
