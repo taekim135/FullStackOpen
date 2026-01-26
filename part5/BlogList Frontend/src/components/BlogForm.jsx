c1e0123ca24a14605959761a3fe4a2901aa2ca02
taekim135 <tkim363@uwo.ca>
2026-01-26 15:44:18 +0900

    Post blog states into compo file

--- part5/BlogList Frontend/src/components/BlogForm.jsx
+++ part5/BlogList Frontend/src/components/BlogForm.jsx
@@ -1,25 +1,39 @@
-const BlogForm = ({onSubmit,
-                    title, 
-                    author, 
-                    url, 
-                    handleTitleChange,
-                    handleAuthorChange,
-                    handleUrlChange}) => {         
+import { useState } from "react"
+
+// just some function passed as para
+const BlogForm = ({createPost}) => {    
+                        
+    const [title, setTitle] = useState("")
+    const [author, setAuthor] = useState("")
+    const [url, setUrl] = useState("")
+
+
+    const addPost = async (event) => {
+        event.preventDefault()
+
+        createPost({ title,  author, url })
+
+        setTitle("")
+        setAuthor("")
+        setUrl("")
+    }
+
+
     return(
         <div>
             <h3>Create New Blog</h3>
-            <form onSubmit={onSubmit}>
+            <form onSubmit={addPost}>
                 <label>
                     Title:
-                    <input type="text" value = {title} onChange={handleTitleChange} required/><br/>
+                    <input type="text" value = {title} onChange={({target}) => {setTitle(target.value)}} required/><br/>
                 </label>
                 <label>
                     Author:
-                    <input type="text" value = {author} onChange={handleAuthorChange} required/><br/>
+                    <input type="text" value = {author} onChange={({target}) => {setAuthor(target.value)}} required/><br/>
                 </label>
                 <label>
                     URL:
-                    <input type="text" value = {url} onChange={handleUrlChange} required/><br/>
+                    <input type="text" value = {url} onChange={({target}) => {setUrl(target.value)}} required/><br/>
                 </label>
                 <button type="submit">Create</button>
             </form>
