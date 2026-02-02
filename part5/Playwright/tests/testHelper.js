c6fd193c5fe81e70fca9946bf3e312af93862567
taekim135 <tkim363@uwo.ca>
2026-02-02 16:34:14 +0900

    create note helper

--- part5/Playwright/tests/testHelper.js
+++ part5/Playwright/tests/testHelper.js
@@ -8,5 +8,16 @@ const loginUser = async (page, username, password) => {
 }
 
 
+const createBlog = async (page, title, author, url) => {
+    await page.getByRole("button", {name: "New Blog"}).click()
 
-export {loginUser}
+    await page.getByLabel("title").fill(title)
+    await page.getByLabel("author").fill(author)
+    await page.getByLabel("url").fill(url)
+
+    await page.getByRole("button", {name: "Create"}).click()
+}
+
+
+
+export {loginUser, createBlog}
