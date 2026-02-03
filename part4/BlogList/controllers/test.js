2e89f19318a8b667cadcee67b26d168aeb0f7f12
taekim135 <tkim363@uwo.ca>
2026-02-03 23:00:16 +0900

    router name change

--- part4/BlogList/controllers/test.js
+++ part4/BlogList/controllers/test.js
@@ -1,13 +1,13 @@
-const loginRouter = require("express").Router()
+const testRouter = require("express").Router()
 const Blog = require("../model/blog")
 const User = require("../model/user")
 
 // /api/testing/reset
-loginRouter.post("/reset", async (request, response) => {
+testRouter.post("/reset", async (request, response) => {
     await Blog.deleteMany({})
     await User.deleteMany({})
 
     response.status(204).end()
 })
 
-module.exports = loginRouter
+module.exports = testRouter
