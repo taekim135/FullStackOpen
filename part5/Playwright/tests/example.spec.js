71929de911c7e67c51ddaaa0518cee82f52b511d
taekim135 <tkim363@uwo.ca>
2026-02-04 00:19:28 +0900

    test for like & delete

--- part5/Playwright/tests/example.spec.js
+++ part5/Playwright/tests/example.spec.js
@@ -59,6 +59,46 @@ describe('Blog app', () => {
       await createBlog(page, "2nd test for new blog", "Tester1", "www.test2.org")
       await expect(page.getByText("2nd test for new blog")).toBeVisible()
     })
+
+    test("a blog can be liked", async ({page}) => {
+      await createBlog(page, "blog for testing like", "Tester1", "www.test2.org")
+      await page.getByRole("button", {name: "Show"}).click()
+      await expect(page.getByTestId("like")).toContainText("0")
+      await page.getByRole("button", {name: "Like"}).click()
+      await expect(page.getByTestId("like")).toContainText("1")
+    })
+
+    test.only("a blog can be deleted by the creator", async ({page, request}) => {
+      // popup message event listener
+      page.on('dialog', async (dialog) => {
+        expect(dialog.message()).toContain("Remove", {exact: false})
+        await dialog.accept()
+      })
+
+      await createBlog(page, "blog for testing delete", "Tester1", "www.test2.org")
+      await page.getByRole("button", {name: "Show"}).click()
+      
+      await page.getByRole("button", {name: "Delete"}).click()
+      await expect(page.locator(".pass")).toContainText("Removed")
+
+      await expect(page.getByText("blog for testing delete")).not.toBeVisible()
+
+      // log in again using a different account to double check the delete button visibility
+      // await page.getByRole("button", {name: "Logout"}).click()
+      
+      // await request.post("/api/users", {
+      //   data: {
+      //     name: 'You',
+      //     username: 'Tester2',
+      //     password: 'testtest'
+      //   }
+      // })
+
+      // await loginUser(page, "Tester2", "testtest")
+      // await page.getByRole("button", {name: "Show"}).click()
+      // await expect(page.getByRole("button", {name: "Delete"})).not.toBeVisible()
+
+    })
   })
 
 
