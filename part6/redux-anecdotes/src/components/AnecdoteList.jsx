cc5762c45e402b239f549f1849726030bc4f0310
taekim135 <tkim363@uwo.ca>
2026-02-13 00:19:42 +0900

    filtering

--- part6/redux-anecdotes/src/components/AnecdoteList.jsx
+++ part6/redux-anecdotes/src/components/AnecdoteList.jsx
@@ -1,11 +1,11 @@
 import { useDispatch, useSelector } from "react-redux"
 import { vote } from "../reducers/anecdoteReducer"
 
-
-
 const AnecdoteList = () => {
     const dispatch = useDispatch()
-    const anecdotes = useSelector(state => state)
+    const anecdotes = useSelector(({filter, anecdotes}) => {
+        return anecdotes.filter(anecdote => anecdote.content.toLowerCase().includes(filter.toLowerCase()))
+    })
 
     return (
         anecdotes.map(anecdote => (
