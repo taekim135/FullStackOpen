60c1dba320e9924148981a6bdf95d3aefe85cc59
taekim135 <tkim363@uwo.ca>
2026-06-03 22:18:08 +0900

    extra comments to refresh

--- part6/query-anecdotes/src/NotificationContext.jsx
+++ part6/query-anecdotes/src/NotificationContext.jsx
@@ -1,6 +1,9 @@
 import { useReducer, createContext, useContext } from "react"
 
 // reducer function
+// the reducer — the rules for how the message var changes
+// pure function - nothing more, nothing less
+// dispatchMessage triggers this function
 const controlNotification = (state, action) => {
     switch (action.type){
         case "SET":
@@ -21,10 +24,15 @@ const NotificationContext = createContext()
 
 // component
 export const NotificationContextProvider = (prop) => {
-            // state, dispatch 
+            // state, dispatch (triggers controlNotification to change message var)
+            // dispatchMessage = doorbell
+            // reducer func = person answering the door and deciding what to do
     const [ message, dispatchMessage ] = useReducer(controlNotification, "")
 
     return (
+        // message and dispatchMessage to other components
+        // app.jsx = dispatchMessage to send action
+        // Notification.jsx = message to display the content
         <NotificationContext.Provider value = {{message, dispatchMessage}}>
             {prop.children}
         </NotificationContext.Provider>
