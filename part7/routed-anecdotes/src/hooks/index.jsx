d192dd9cf8b7540751e2663ce40290c8d56e3ed2
taekim135 <tkim363@uwo.ca>
2026-06-09 14:18:55 +0900

    7.4 done - anecdotes fetch hook

--- part7/routed-anecdotes/src/hooks/index.jsx
+++ part7/routed-anecdotes/src/hooks/index.jsx
@@ -1,4 +1,5 @@
-import { useState } from 'react'
+import { useState,useEffect } from 'react'
+import anecdoteService from "../services/anecdotes"
 
 export const useField = (type) => {
   const [value, setValue] = useState('')
@@ -15,4 +16,17 @@ export const useField = (type) => {
     inputs: {type, value, onChange},
     reset
   }
+}
+
+export const useAnecdotes = () => {
+    const [anecdotes, setAnecdotes] = useState([])
+
+    useEffect(() => {
+        anecdoteService.getAll().then(data => setAnecdotes(data))
+    }, [])
+
+    return {
+        anecdotes
+    }
+
 }
